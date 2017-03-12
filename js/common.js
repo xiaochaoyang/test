7a9c6320f5db4989467ceb7ee7c46816d22a6313
肖朝阳1 <534600451@qq.com>
2017-03-12 21:09:20 +0800

    导航栏

---
+++ js/common.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	switchtabs();
+})
+var isPoll = true;
+function switchtabs (){
+	var indicater = $("#indicater");
+	var tabs = $(".navigation li");
+	tabs.mouseover(function(){
+		for(var i=0;i<tabs.length;i++){
+			if(tabs[i]==this){
+				indicater.animate({left:String(i*120)+'px'},'fast');
+			}
+		}
+	})
+	tabs.parent().mouseleave(function(){
+		indicater.animate({left:0+'px'},'fast');
+	})
+}
