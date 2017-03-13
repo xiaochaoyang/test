84c628353bc73b6c3752f224607ebbb184ebcabf
xcy <kjs_shan@shandeMac-mini.local>
2017-03-13 11:54:16 +0800

    no message

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var ispoll = true;
+var index = 0;
+function rotate(index){
+	if(index>4){
+		
+	}
+	if(isPoll){
+		var pictures = $("#pictureRotate li");
+		for (var i=0;i<pictures.length;i++) {
+			var li = $('#pic'+i);
+			if(i==index){
+				li.animate({opacity:1},1000);
+			}else{
+				li.animate({opacity:0},1000);
+			}
+		}
+	}
+}
+function pollPlay(){
+  	index++;
+	rotate(index%4);
+
+}
+$(document).ready(function(){
+	setInterval('pollPlay()',5000);
+   
+});
