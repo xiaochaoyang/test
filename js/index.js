46fd64af5b91a38bfe2551a3eb7e94c062ebc840
xcy <kjs_shan@10.0.0.16>
2017-03-14 19:09:04 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 var ispoll = true;
 var index = 0;
-function rotate(index){
-	if(index>4){
-		
-	}
-	if(isPoll){
-		var pictures = $("#pictureRotate li");
+function rotate(currentIndex){
+	if(ispoll){
+		var pictures = $("#tab_body li");
+		var indicater = $("#tab_head .indicater");
 		for (var i=0;i<pictures.length;i++) {
 			var li = $('#pic'+i);
-			if(i==index){
+			if(i==currentIndex){
 				li.animate({opacity:1},1000);
+				indicater.animate({left:String(80*i)+'px'},1000);
 			}else{
 				li.animate({opacity:0},1000);
 			}
+			
 		}
 	}
 }
@@ -23,5 +23,19 @@ function pollPlay(){
 }
 $(document).ready(function(){
 	setInterval('pollPlay()',5000);
-   
+    selectTab();
 });
+function selectTab(){
+	var tabs = $('#tab_head li');
+	tabs.mouseover(function(){
+		for (var i=0;i<tabs.length;i++) {
+			if(tabs[i]==this){
+				index = i;
+				rotate(index);
+				ispoll = false;
+			}
+		}
+	}).parent().mouseleave(function(){
+		ispoll = true;
+	})
+}
